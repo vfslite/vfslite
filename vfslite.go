7e859aabe2f22eb22f41bf2b604651ac69d89263
Alex Gaetano Padula <me@alexpadula.com>
2025-03-08 15:18:12 -0500

    start of vfslite main library (go) structures and constants

--- vfslite.go
+++ vfslite.go
@@ -15,3 +15,39 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 package vfslite
+
+import "vfslite/disk"
+
+// Block types
+const (
+	BlockTypeRoot      = 1
+	BlockTypeDirectory = 2
+	BlockTypeFile      = 3
+	BlockTypeData      = 4
+	HeaderSize         = 9    // size of Type(1) + ChildCount(2) + DataSize(4) + MetaSize(2)
+	ReferenceSize      = 8    // Size of each block reference (uint64)
+	DebugEnabled       = true // Set to true to enable debug output
+	SuperblockNum      = 0    // Block 0 is reserved for the superblock
+)
+
+// BlockHeader defines block specific metadata at the beginning of each block
+type BlockHeader struct {
+	Type       uint8  // Type of block (e.g., directory, file, etc.)
+	ChildCount uint16 // Number of child references
+	DataSize   uint32 // Size of data in this block (excluding header and references)
+	MetaSize   uint16 // Size of metadata in bytes
+}
+
+// Metadata defines common metadata for all file and directory blocks
+type Metadata struct {
+	CreatedOn  int64             `json:"createdOn"`  // Unix timestamp for creation time
+	ModifiedOn int64             `json:"modifiedOn"` // Unix timestamp for last modification
+	Extra      map[string]string `json:"extra"`      // Optional extra metadata as key-value pairs
+} // JSON works fine :)
+
+// VFSLite main structure for the virtual filesystem
+type VFSLite struct {
+	disk      *disk.Disk // Underlying disk structure
+	blockSize uint       // Size of each block in bytes (configured by user)
+	rootBlock uint64     // The entry point to our disk structure
+}
